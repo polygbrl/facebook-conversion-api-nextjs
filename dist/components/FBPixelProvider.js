0d4fa483e5dc003420a2f98e2fbb3d1753c5ad62
polygbrl <gab.ggh@gmail.com>
2024-03-01 13:06:40 +0100

    Update FBPixelProvider.js

--- dist/components/FBPixelProvider.js
+++ dist/components/FBPixelProvider.js
@@ -26,10 +26,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importStar(require("react"));
 const router_1 = require("next/router");
 const conversion_api_1 = require("../conversion-api");
-const FBPixelProvider = ({ children }) => {
+const FBPixelProvider = ({ children, triggerPageView }) => {
     const router = (0, router_1.useRouter)();
     (0, react_1.useEffect)(() => {
-        (0, conversion_api_1.fbPageView)();
+        if (triggerPageView)
+            (0, conversion_api_1.fbPageView)();
         router.events.on('routeChangeComplete', conversion_api_1.fbPageView);
         return () => {
             router.events.off('routeChangeComplete', conversion_api_1.fbPageView);
